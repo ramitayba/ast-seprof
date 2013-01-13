ca998cf5413d75ccbb9a1fe1eaf9bb8dae42c91c
fa.quran <fa.quran@f2861043-9463-9c56-2092-a30b48ef11cc>
2013-01-13 15:51:27 +0000

    new folder and classes.

---
+++ trunk/ast/controller/userController.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * This is the userController
+ * Designed and Developed by SEProf Team
+ * Copyright (c) 2013 SEProf Inc.
+ * http://seprof.com/
+ * 
+ */
+
+?>
---
+++ trunk/ast/helper/variables.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+global $root,$action,$databases,$routes;
+
+
